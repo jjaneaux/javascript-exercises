b484b7f19cc1f37a5a975abf98efb58232bbcb15
jjaneaux <47292559+jjaneaux@users.noreply.github.com>
2025-03-25 21:37:14 -0500

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringReversed = "";
+    let i = string.length;
+
+    while (i > 0) {
+        stringReversed += string[i -1];
+        i--;
+    }
+
+    return stringReversed;
 
 };
+
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
