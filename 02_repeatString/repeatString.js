e6e0ffb20d64e27c373a31dab43604c59093d625
jjaneaux <47292559+jjaneaux@users.noreply.github.com>
2025-03-25 20:47:30 -0500

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let stringRepeated = "";
+    let i = 0;
+
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        while (i < num) {
+            stringRepeated += string;
+            i++;
+        }
+    }
+
+    return stringRepeated;
 
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
