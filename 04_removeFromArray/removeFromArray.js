e8f37f6528d56760d282122dad00a78055070ae1
jjaneaux <47292559+jjaneaux@users.noreply.github.com>
2025-03-25 22:53:35 -0500

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    let newArray = array;
+    let removeIndex = newArray.indexOf(toRemove);
+
+    // Remove a single instance
+    // newArray.splice(removeIndex, 1);
+
+    // Remove all isntances
+    return array.filter(item => !toRemove.includes(item));
 };
+
+let numbers = [1, 2, 3, 4, 5, 3, 4, 5, 3, "hey"];
+removeFromArray(numbers, 1, "hey", 5);
+console.log(numbers);
 
 // Do not edit below this line
 module.exports = removeFromArray;
